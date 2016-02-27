77f8ff2daadfed1213abb6507acb97f2a9522966
KevinGtz <kazevtrinid@gmail.com>
2016-02-26 22:26:24 -0600

    subiendo todos los archivos de emergency app

---
+++ emergency/emergencyapp/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class formulario (models.models)
+
+# Create your models here.
---
+++ emergency/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kevingtz/hackathon/emergency/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
